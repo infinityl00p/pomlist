b14a99326fe03f696db00032f0f608f825b92f45
James Gill <james@jgdev.me>
2018-08-30 13:04:53 -0700

    plugged in clock with incrementing seconds

--- src/components/pomodoro/Pomodoro.js
+++ src/components/pomodoro/Pomodoro.js
@@ -1,23 +1,14 @@
 import React, { Component } from 'react';
-import CircularProgressbar from 'react-circular-progressbar';
 import Timer from './Timer';
 import Button from '../reusable/Button';
-import 'react-circular-progressbar/dist/styles.css';
 import '../../assets/css/Pomodoro.css';
 
 class Pomodoro extends Component {
     render() {
-        const percentage = 66;
         return(
             <div className="pomodoro">
-                <div className="progress-bar">
-                    <CircularProgressbar
-                        percentage={percentage}
-                        text={`25:00`}
-                    />
-                </div>
 
-                {/* <Timer /> */}
+                <Timer />
                 {/* <Button
                     buttonContent={<i className="fas fa-stop"></i>}
                     className={'button--icon button--stop color--red'}
